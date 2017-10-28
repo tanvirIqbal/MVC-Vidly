79e86a9064ecd7651246bb9593ce5df3f83973f2
tanviriqbal <tanviriqbal4u@live.com>
2017-10-28 19:41:27 +0600

    Add Custom Validation

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -18,6 +18,7 @@ namespace Vidly.Models
         [Display(Name="Membership Type")]
         public int MembershipTypeId { get; set; }
         [Display(Name="Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
 }
---
+++ Vidly/Vidly/Models/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Enums
+    {
+    }
+    public enum EnumMembershipType
+    {
+        None = 0,
+        PayAsYouGo = 1,
+        Monthly = 2,
+        Quarterly = 3,
+        Yearly = 4
+    }
+}
