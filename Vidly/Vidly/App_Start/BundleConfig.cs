94e58407182304039efb01ea7f9517bda0dedb30
tanviriqbal <tanviriqbal4u@live.com>
2017-11-18 10:15:18 +0600

    Add jquery.datatables in BundleConfig

--- Vidly/Vidly/App_Start/BundleConfig.cs
+++ Vidly/Vidly/App_Start/BundleConfig.cs
@@ -12,7 +12,9 @@ namespace Vidly
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js",
                       "~/Scripts/bootbox.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/DataTables/jquery.dataTables.js",
+                      "~/Scripts/DataTables/dataTables.bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,6 +26,7 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
